d20f67f2382c65aa997b0f404cccda5c62eb758b
Flemin1235 <flemin1203@gmail.com>
2026-02-26 20:58:53 +0530

    added a temporary lstm

--- stock.py
+++ stock.py
@@ -13,14 +13,17 @@ nifty50_tickers = [
     "ADANIPORTS.NS", "ASIANPAINT.NS", "AXISBANK.NS", "BAJAJ-AUTO.NS", "BAJFINANCE.NS",
     "BAJAJFINSV.NS", "BPCL.NS", "BHARTIARTL.NS", "BRITANNIA.NS", "CIPLA.NS",
     "COALINDIA.NS", "DIVISLAB.NS", "DRREDDY.NS", "EICHERMOT.NS", "GRASIM.NS",
-    "HCLTECH.NS", "HDFC.NS", "HDFCBANK.NS", "HDFCLIFE.NS", "HEROMOTOCO.NS",
+    "HCLTECH.NS", "HDFCLIFE.NS", "HDFCBANK.NS", "HDFCLIFE.NS", "HEROMOTOCO.NS",
     "HINDALCO.NS", "HINDUNILVR.NS", "ICICIBANK.NS", "INDUSINDBK.NS", "INFY.NS",
     "ITC.NS", "JSWSTEEL.NS", "KOTAKBANK.NS", "LT.NS", "M&M.NS",
     "MARUTI.NS", "NESTLEIND.NS", "NTPC.NS", "ONGC.NS", "POWERGRID.NS",
     "RELIANCE.NS", "SBILIFE.NS", "SHREECEM.NS", "SBIN.NS", "SUNPHARMA.NS",
-    "TCS.NS", "TATACONSUM.NS", "TATAMOTORS.NS", "TATASTEEL.NS", "TECHM.NS",
+    "TCS.NS", "TATACONSUM.NS", "TMPV.NS", "TATASTEEL.NS", "TECHM.NS",
     "TITAN.NS", "ULTRACEMCO.NS", "UPL.NS", "WIPRO.NS", "ADANIENT.NS"
 ]
+
+main_df = pd.DataFrame()
+
 for ticker in nifty50_tickers:
     print(f"Fetching {ticker}...")
 
@@ -32,22 +35,22 @@ for ticker in nifty50_tickers:
         end=end_date.strftime("%Y-%m-%d")
     )
 
-    if df.empty:
-        print(f"⚠ No data for {ticker}")
-        continue
-
-    
     company_name = stock.info.get("longName", ticker)
 
     # Clean filename (remove special characters)
     company_name = re.sub(r"[^\w\s-]", "", company_name)
     company_name = company_name.replace(" ", "_")
 
-    # Save CSV
-    filename = f"{company_name}_5yr.csv"
-    df.to_csv(filename)
+    df['Company'] = company_name
+    if df.empty:
+        print(f"⚠ No data for {ticker}")
+        continue
 
-    print(f"✔ Saved: {filename}")
+    main_df = pd.concat([main_df, df])
+
+    # Save CSV
+    
+main_df.to_csv('stock_data_5yrs.csv')
 
 print("\n✅ All NIFTY 50 data saved by company name.")
 
